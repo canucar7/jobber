e15a332518e1a523a615876a41b0087df0577f60
burakersen <burak.ersen@sekizbit.com.tr>
2023-04-06 16:47:12 +0300

    [WIP]

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:jobfinder/pages/login.dart';
+import 'package:jobfinder/pages/auth/login.dart';
 import 'package:jobfinder/pages/slider.dart';
 import 'package:jobfinder/provider/UserProvider.dart';
 import 'package:provider/provider.dart';
