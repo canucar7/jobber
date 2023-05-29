832c4308aad48448b2c0471d5b3509f49a008b09
Can Ucar <canucar7@hotmail.com>
2023-05-30 00:18:03 +0300

    [WIP] remove license of template

--- lib/widget/elevated_button.dart
+++ lib/widget/elevated_button.dart
@@ -1,11 +1,4 @@
-/*
-  Authors : flutter_ninja (Flutter Ninja)
-  Website : https://codecanyon.net/user/flutter_ninja/
-  App Name : JobFinder Flutter Template
-  This App Template Source code is licensed as per the
-  terms found in the Website https://codecanyon.net/licenses/standard/
-  Copyright and Good Faith Purchasers © 2022-present flutter_ninja.
-*/
+
 import 'package:flutter/material.dart';
 import '../components/styles.dart';
 
